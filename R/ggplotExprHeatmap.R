20759f2d76cf7e6338eb9f14b97cb3172d47d2f0
Richard White <richard.white@sanger.ac.uk>
2017-02-09 14:35:11 +0000

    Added external package namespaces for function calls

--- R/ggplotExprHeatmap.R
+++ R/ggplotExprHeatmap.R
@@ -1,28 +1,29 @@
 #' ggplotExprHeatmap
-#' 
+#'
 #' \code{ggplotExprHeatmap} returns a heatmap for the supplied matrix
-#'    
+#'
 #' @param exprMatrix A numeric matrix of gene expression values
 #' @param legendPosition character (one of 'left'[default], 'right', 'bottom', 'top')
-#' 
+#'
 #' @return a ggplot heatmap object
-#' 
+#'
 #' @examples
 #' ggplotExprHeatmap( matrix, legendPosition = 'right')
-#' 
+#'
 #' @export
 ggplotExprHeatmap <- function( exprMatrix, legendPosition = 'left' ){
   # reshape data for heatmap
-  exprMatrix.m <- melt(exprMatrix)
+  exprMatrix.m <- reshape2::melt(exprMatrix)
   colnames(exprMatrix.m) <- c("Gene", "Sample", "Value")
   # reorder levels of gene
   exprMatrix.m$Gene <- factor( exprMatrix.m$Gene,
                                levels = rev(levels(exprMatrix.m$Gene)) )
-  
-  Heatmap <- ggplot(data = exprMatrix.m) + 
-    geom_raster( aes( y = Gene, x = Sample, fill = Value ) ) + 
-    scale_fill_gradientn( colours = c("blue", "yellow", "red") ) + 
-    theme_void() + theme( legend.position=legendPosition,
-                          legend.title = element_text(colour="black" ) )
+
+  Heatmap <- ggplot2::ggplot(data = exprMatrix.m) +
+    ggplot2::geom_raster( ggplot2::aes( y = Gene, x = Sample, fill = Value ) ) +
+    ggplot2::scale_fill_gradientn( colours = c("blue", "yellow", "red") ) +
+    ggplot2::theme_void() +
+    ggplot2::theme( legend.position=legendPosition,
+                    legend.title = ggplot2::element_text(colour="black" ) )
   return( Heatmap )
 }
